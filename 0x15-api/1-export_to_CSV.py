848e0c732b4f4e6e2c83645716fe13ba06cd63ec
steffanynaranjov <steffany.naranjo@gmail.com>
2021-02-01 23:43:19 +0000

    export cvs file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Python script that using a REST API, for a given employee ID,
+returns information about list progress"""
+
+if __name__ == '__main__':
+    import sys
+    import csv
+    import requests
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_id = sys.argv[1]
+    us_id = requests.get(url + 'users?', params={'id': user_id}).json()
+    to_do = requests.get(url + 'todos?', params={'userId': user_id}).json()
+    employee = us_id[0].get("username")
+
+    with open("{}.csv".format(user_id), "w", newline="") as cvs_f:
+        writer = csv.writer(cvs_f, quoting=csv.QUOTE_ALL, delimiter=',',
+                            quotechar='"')
+        [writer.writerow([user_id, employee, task.get("completed"),
+                          task.get("title")]) for task in to_do]
