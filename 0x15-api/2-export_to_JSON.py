c19e923d044a8b99fea1a22391b3d8e158d45e43
steffanynaranjov <steffany.naranjo@gmail.com>
2021-02-01 23:54:28 +0000

    export a .json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Python script that using a REST API, for a given employee ID,
+returns information about list progress"""
+
+if __name__ == '__main__':
+    import sys
+    import json
+    import requests
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_id = sys.argv[1]
+    us_id = requests.get(url + 'users?', params={'id': user_id}).json()
+    to_do = requests.get(url + 'todos?', params={'userId': user_id}).json()
+    employee = us_id[0].get("username")
+
+    with open("{}.json".format(user_id), "w") as jsonf:
+        json.dump({user_id: [{"task": task.get("title"),
+                              "completed": task.get("completed"),
+                              "username": employee} for task in to_do]}, jsonf)
